719bf41129b6361486c86ff311371d2dcde79fef
AndresChila <AndresChila@DESKTOP-BK0R245>
2019-08-24 15:05:18 -0500

    Correccion de errores, se completa documentacion

--- src/main/java/com/udec/caracteresrepetidos/Letras.java
+++ src/main/java/com/udec/caracteresrepetidos/Letras.java
@@ -10,28 +10,31 @@ import java.util.Scanner;
 /**
  *
  * @author AndresChila, Tatiana Moreno
+ * 
+ * Clase que se encarga de analizar el primer caracter que menos se ha repedito en un string 
  */
 public class Letras {
 
     //variable donde se guarda la cadena entrante
-
     private String palabra;
     /**
      * array de caracteres en el cual se guardan los caracteres separados de la
      * frase
      */
     private char[] caracteres;
+    /**array que nos sirve para llevar cuenta de cuantas veces lleva repetida 
+    *una letra hasta esa posicion al momento de recorrer la frase
+    */
+    private byte[] acumuladorLetra;
     /**
      * variable que nos sirve para llevar control de la posicion de la palabra
      * que estamos analizando
      */
     private byte contador = 1;
     //variable que lleva la letra que menos se repite
-    private char identificadorLetra;
-    //variable para llevar cantidad de la letra que se esta analizando
-    private byte acumuladorLetra = 0;
+    private char identificadorLetraMenor;
     //variable que sirve para llevar cantidad de la letra siguiente
-    private byte cantidad;
+    private byte cantidad=50;
     //variable para hacer que solo entre una vez al if
     private boolean soloPrimera;
 
@@ -48,14 +51,18 @@ public class Letras {
         palabra.replace(" ", "");
         //Se crea el array con la misma longitud de la frase entrante 
         caracteres = new char[palabra.length()];
+        acumuladorLetra = new byte [palabra.length()];
         //Convierte la frase a un array de caracteres
         caracteres = palabra.toCharArray();
-
+        inicializaArray();
         analizaCaracteres();
+
     }
 
     /**
-     *
+     *Metodo que compara el array una letra vs todas las demas y cuenta la 
+     *cantidad que se repite cada una en el array y guarda esa cantidad en 
+     * acumuladorLetra[]
      */
     public void analizaCaracteres() {
         System.out.println("Frase: " + palabra);
@@ -63,16 +70,37 @@ public class Letras {
             System.out.println("Leyendo: " + caracteres[i] + " ");
             for (byte j = 0; j < palabra.length(); j++) {
                 if (caracteres[i] == caracteres[j]) {
-                    acumuladorLetra++;
+                    acumuladorLetra[i]++;
                 }
+                //System.out.print(acumuladorLetra[j]);
             }
-            if (acumuladorLetra == 1 && soloPrimera == false) {
-                System.out.println("La primera letra que no se repite: " + caracteres[i] + " ");
-                soloPrimera = true;
-            } else if (acumuladorLetra == 1 && soloPrimera == true) {
-                System.out.println("La letra que no se repite: " + caracteres[i] + " ");
-            }
-            acumuladorLetra = 0;
+            imprimeMenorVecesRepetido();
+            contador++;
         }
     }
+    /**
+     *Metodo que inicializa todas las posiciones del array en cero
+     */
+    public void inicializaArray(){
+        for(byte i =0; i< palabra.length();i++){
+            acumuladorLetra[i]=0;
+        }
+    }
+    /**
+     * metodo que compara cual es el que menos se repite en el array acumuladorLetra[]
+     * e imprime el menor
+     */
+    public void imprimeMenorVecesRepetido(){
+        contador--;
+        for(byte i = contador;i< palabra.length();i++){
+            for(byte j = contador;j<palabra.length();j++){
+                if(acumuladorLetra[i]<cantidad && acumuladorLetra[i]!=0){
+                    cantidad=acumuladorLetra[i];
+                    identificadorLetraMenor = caracteres[i];
+                }
+            }
+        }
+        System.out.println(" la primera letra que menos se repite es: "+ identificadorLetraMenor);
+    }
 }
+
